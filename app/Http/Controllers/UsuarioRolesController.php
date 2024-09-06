1ace718dd61e7fc694157183f5ac378f7acc7e9d
JorgeParedes77Kick <jorge.paredes@usach.cl>
2024-09-06 18:19:32 -0400

    fix files usuarios

--- app/Http/Controllers/UsuarioRolesController.php
+++ app/Http/Controllers/UsuarioRolesController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
-class UsuarioController extends Controller {
+class UsuarioRolesController extends Controller {
     /**
      * Display a listing of the resource.
      *
